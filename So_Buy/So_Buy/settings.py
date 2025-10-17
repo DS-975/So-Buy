b3b8baf7c32194bf788927db6e7e7848281f26a2
Danil <danilsavrin93@gmail.com>
2025-10-17 20:18:37 +0300

    Настроена СУБД. для разработки SQLite, для продакшена PostgreSQL.

--- So_Buy/So_Buy/settings.py
+++ So_Buy/So_Buy/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'So_Buy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('DJANGO_ENV') == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'So_Buy',
+            'USER': 'postgres',
+            'PASSWORD': '15426378',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
