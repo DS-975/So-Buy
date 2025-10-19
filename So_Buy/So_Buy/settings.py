2c147d5ca4413d2eb4cecf087c11142b72acf6a6
Danil <danilsavrin93@gmail.com>
2025-10-19 16:38:30 +0300

    Добавлен путь к приложению registration

--- So_Buy/So_Buy/settings.py
+++ So_Buy/So_Buy/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'rest_framework',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
