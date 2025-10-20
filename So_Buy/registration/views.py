4d7b7182df7673dd4dc5c1f74c6d93f4a8b3f9ee
Danil <danilsavrin93@gmail.com>
2025-10-20 08:41:47 +0300

    Исправлены ошибки и добавлены комментарии

--- So_Buy/registration/views.py
+++ So_Buy/registration/views.py
@@ -5,24 +5,38 @@ from .forms import AuthenticationForm
 
 # Create your views here.
 def check_email_and_password(request):
+    # Обработка POST-запроса - когда пользователь отправил форму
     if request.method == 'POST':
+        # Создание пустой формы для отображения
         form = AuthenticationForm(request.POST)
+        # Проверяем валидность формы
         if form.is_valid():
+            # Извлекаем очищенные данные из формы
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
 
-            user = authenticate(request, email=email, password=password)
+            # Аутентифицируем пользователя по email
+            # ПЕРЕДАЕМ email как username, потому что стандартный authenticate работает с username
+            user = authenticate(request, username=email, password=password)
 
+            # Проверяем, что пользователь найден и пароль верный
             if user is not None:
+                # Логиним пользователя (Создаём сессию)
                 login(request, user)
-                return render(request, 'templates/flatpages/success_authentifications.html', {'email': email, 'password': password})
+                # Перенаправляем пользователя на станицу с текстом успешной аутентификации
+                return render(request, 'flatpages/success_authentifications.html', {'email': email, 'password': password})
             else:
+                # Если аутентификация не удалась, вызываем ошибку
                 form.add_error(None, 'Неверная электронная почта или пароль')
-        else:
-            form = AuthenticationForm()
-        return render(request, 'templates/flatpages/error_authentifications.html', {'form': form})
+
+        return render(request, 'flatpages/error_authentifications.html', {'form': form})
 
 
+    else:
+        # Если форма невалидна (например, некорректный email)
+        # Показываем ту же форму с сообщениями об ошибках валидации
+        form = AuthenticationForm()
+        return render(request, 'flatpages/error_authentifications.html', {'form': form})
 
 
 
