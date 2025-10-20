b2758169ef42453bbe807a094a7bd7e504e04734
Danil <danilsavrin93@gmail.com>
2025-10-20 08:10:19 +0300

    Изменён путь с success_autentifications на login

--- So_Buy/registration/urls.py
+++ So_Buy/registration/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('success_authentification/', views.check_email_and_password, name='check_email_and_password')
+    path('login/', views.check_email_and_password, name='login')
 ]
