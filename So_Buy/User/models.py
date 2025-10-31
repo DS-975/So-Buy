dbbd5bd6bcc5e05996368b49781c66b0f2890aca
Danil <danilsavrin93@gmail.com>
2025-10-31 14:40:44 +0300

    Новая структура БД

--- So_Buy/User/models.py
+++ So_Buy/User/models.py
@@ -5,6 +5,22 @@ from django.core.exceptions import ValidationError
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='customuser_set',  # Уникальное имя
+        related_query_name='customuser',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='customuser_set',  # Уникальное имя
+        related_query_name='customuser',
+    )
     email = models.EmailField(unique=True, verbose_name="email", error_messages={"unique": "Пользователь с данной электронной почтой уже был зарегистрирован"}, blank=False)
     phone = models.CharField(max_length=12, unique=True, verbose_name='phone', blank=False)
     balance = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
