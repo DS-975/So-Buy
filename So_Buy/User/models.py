f0239fbe8fd31aa54b358be0e1b6bdd7e996ca89
Danil <danilsavrin93@gmail.com>
2025-11-20 19:35:17 +0300

    Убраны поля email и password, убрана валидация пароля, добавлена проверка на superuser

--- So_Buy/User/models.py
+++ So_Buy/User/models.py
@@ -8,25 +8,16 @@ from django.core.exceptions import ValidationError
 
 # Create your models here.
 class CustomUser(AbstractUser):
-
-
-    name = models.CharField(max_length=20, null=True)
-    surname = models.CharField(max_length=20, null=True)
-    password = models.CharField(max_length=128, validators=[MinLengthValidator(8), MaxLengthValidator(128)])
-    email = models.EmailField(unique=True, verbose_name="email", blank=False)
-    phone = models.CharField(max_length=12, unique=True, verbose_name='phone', blank=False)
+    name = models.CharField(max_length=20, null=True, blank=True)
+    surname = models.CharField(max_length=20, null=True, blank=True)
+    phone = models.CharField(max_length=12, unique=True, verbose_name='phone', blank=True)
     balance = models.DecimalField(max_digits=15, decimal_places=2, validators=[MinValueValidator(0)], default=0.00)
 
 
-    def check_password(self):
-        if len(self.password) < 8:
-            raise ValidationError('Длина пароля не должна быть меньше 8 символов')
-        if len(self.password) > 128:
-            raise ValidationError('Длина пароля не должна быть больше 128 символов')
 
     def check_phone(self):
         if len(self.phone) != 12:
-            raise ValidationError('Номер должен состоять из 12 символов')
+             raise ValidationError('Номер должен состоять из 12 символов')
         if not self.phone.startswith('+'):
             raise ValidationError('Номер должен начинаться с +')
         if not self.phone[1:].isdigit():
@@ -35,8 +26,8 @@ class CustomUser(AbstractUser):
 
 
     def save(self, *args, **kwargs):
-        self.check_password()
-        self.check_phone()
+        if not self.is_superuser and self.phone:
+            self.check_phone()
         super().save(*args, **kwargs)
 
 
